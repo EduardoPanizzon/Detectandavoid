421139f5b2d89ce1ed129caa19175b9f5df5bbfe
EduardoPanizzon <113205827+EduardoPanizzon@users.noreply.github.com>
2024-10-30 19:34:51 -0300

    Add files via upload

---
+++ build/CMakeFiles/detectandavoid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detectandavoid.dir/src/main.cpp.o"
+  "CMakeFiles/detectandavoid.dir/src/main.cpp.o.d"
+  "detectandavoid"
+  "detectandavoid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detectandavoid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
